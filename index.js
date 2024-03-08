3f5abd2d0b23c6b58e672f64924486921a890e3b
wellheythereMJ <abetterplace@outlook.com>
2024-03-07 22:06:09 -0700

    setup questions

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
+const {Square, Triangle, Circle} = require('./shapes.js');
 // user inquirer to ask questions
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'logo',
+      message: 'Enter LOGO initials (no more than three characters)',
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'What text color do you want? (enter hex code)',
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape do you want? ()',
+      choices: ['circle', 'square', 'triangle'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'What shape color do you want? (enter hex code)', 
+    },
+  ])
+  .then(({logo, textColor, shape, shapeColor}) => {
+    let shapeSelection;
+    if (shape === 'circle') {
+      shapeSelection = new Circle();
+    }
+    else if (shape ==='square') {
+      shapeSelection = new Square();
+    } else {
+      shapeSelection = new Triangle();
+    }
+    shapeSelection.setShapeColor(shapeColor);
+
+    // set shape color, text, and text color
+// shapeSelection.setShapeColor(shapeColor);
+    // 
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
 
